3c16fcd4578ca0394eaea034a0be8ee25400a6a8
Paul Kinlan <paul.kinlan@gmail.com>
2014-05-14 14:14:43 +0100

    Adding psi results for some of the top sites

--- parse-score.py
+++ parse-score.py
@@ -7,8 +7,6 @@ import urllib
 import sys
 import fileinput
 
-url = "https://www.googleapis.com/pagespeedonline/v1/runPagespeed?key=AIzaSyBfFgrmdXPeTDseNiODoMkrdvCWSsVuGr0&strategy=mobile&screenshot=true&rule=AvoidLandingPageRedirects&rule=ServerResponseTime&rule=MinimizeRenderBlockingResources&rule=PrioritizeVisibleContent&rule=EnableGzipCompression&rule=InlineRenderBlockingCss&rule=PreferAsyncResources&rule=uselegiblefontsizes&rule=sizetaptargetsappropriately&rule=avoidplugins&rule=configureviewport&url="
-
 for line in fileinput.input():
   try:
     obj = json.loads(line)
