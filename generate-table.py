34aa83d7e29164f3f9f1404a2265b249acf26dbc
Paul Kinlan <paul.kinlan@gmail.com>
2014-05-19 10:46:37 +0100

    Fixing up table generation, regenerating markdown and adding in new source

--- generate-table.py
+++ generate-table.py
@@ -7,8 +7,11 @@ import urllib
 import sys
 import fileinput
 
+
+pagespeed_url = "https://developers.google.com/speed/pagespeed/insights/?url="
+
 for line in fileinput.input():
   (domain, score) = line.split("\t")
   print "## %s\n" % (domain)
-  print "*  Score: %s" % (score)
+  print "*  Score: [%s](%s%s)" % (score, pagespeed_url, domain)
   sys.stdout.flush()
