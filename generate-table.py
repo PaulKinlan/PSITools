811e5f003e56deef3782525f2eb19474c27e4042
Paul Kinlan <paul.kinlan@gmail.com>
2014-05-19 10:55:46 +0100

    Fixing generate-table to add new lines

--- generate-table.py
+++ generate-table.py
@@ -13,5 +13,5 @@ pagespeed_url = "https://developers.google.com/speed/pagespeed/insights/?url="
 for line in fileinput.input():
   (domain, score) = line.split("\t")
   print "## %s\n" % (domain)
-  print "*  Score: [%s](%s%s)" % (score, pagespeed_url, domain)
+  print "*  Score: [%s](%s%s)\n" % (score, pagespeed_url, domain)
   sys.stdout.flush()
