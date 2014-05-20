b5092bd53dedbe0821ebb0b40f71baec8afcdea8
Paul Kinlan <paul.kinlan@gmail.com>
2014-05-20 08:26:38 +0100

    Making run.py threaded and testing the output on news sites

--- run.py
+++ run.py
@@ -6,13 +6,48 @@ import urllib2
 import urllib
 import sys
 import fileinput
+import Queue
+import threading
 
 url = "https://www.googleapis.com/pagespeedonline/v1/runPagespeed?key=AIzaSyBfFgrmdXPeTDseNiODoMkrdvCWSsVuGr0&strategy=mobile&screenshot=true&rule=AvoidLandingPageRedirects&rule=ServerResponseTime&rule=MinimizeRenderBlockingResources&rule=PrioritizeVisibleContent&rule=EnableGzipCompression&rule=InlineRenderBlockingCss&rule=PreferAsyncResources&rule=uselegiblefontsizes&rule=sizetaptargetsappropriately&rule=avoidplugins&rule=configureviewport&url="
 
-for line in fileinput.input():
-	try:
-		res = urllib2.urlopen(url + urllib.quote(line.rstrip()))
-		print "%s" % (json.dumps(json.loads(res.read())))
-		sys.stdout.flush()
-	except:
-	  pass
+request_queue = Queue.Queue()
+max_threads = 5
+
+class UrlFetchThread(threading.Thread):
+  def __init__(self, queue):
+    threading.Thread.__init__(self)
+    self.queue = queue
+
+  def run(self):
+    while True:
+      #grabs host from queue
+      host = self.queue.get()
+
+      #grabs urls of hosts and then grabs chunk of webpage
+      res = urllib2.urlopen(url + urllib.quote(host.rstrip()))
+      print json.dumps(json.loads(res.read()))
+
+      #signals to queue job is done
+      self.queue.task_done()
+
+
+def main():
+
+  # Create some threads and add our queue
+  for i in range(max_threads):
+      t = UrlFetchThread(request_queue)
+      t.setDaemon(True)
+      t.start()
+
+  #populate queue with data
+  for host in fileinput.input():
+      request_queue.put(host)
+
+
+  #wait on the requests to complete until everything has been processed
+  request_queue.join()
+
+main()
+
+
